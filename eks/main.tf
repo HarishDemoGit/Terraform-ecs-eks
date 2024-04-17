c01cd6e070590001afa7500cf05aedfb71f79941
HarishDemoGit <harishdurai2@gmail.com>
2024-04-17 16:26:01 +0530

    demo19

--- eks/main.tf
+++ eks/main.tf
@@ -1,26 +1,7 @@
 
 # Module for istio in kubernetes using helm
 
-# module "istio" {
-#   source  = "truemark/istio/kubernetes"
-#   version = "0.0.5"
-#   # insert the 1 required variable here
-#   vpc_id = "Module.Network.vpc_id"
-#   # depends_on = [ "aws eks update-kubeconfig --region us-east-1 --name demo_eks_cluster" ]
-# }
 
-# resource "null_resource" "update_kubeconfig" {
-#     depends_on = [ aws_eks_cluster.eks_main_cluster ]
-#   # This null resource will run the aws eks update-kubeconfig command
-#   # before applying the Istio module.
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-
-#   provisioner "local-exec" {
-#     command = "aws eks update-kubeconfig --region us-east-1 --name demo_eks_cluster"
-#   }
-# }
 
 # The above module is working perfectly
 
@@ -73,3 +54,24 @@ resource "aws_eks_node_group" "eks_nodeGroup" {
 #   service_account = "example-sa"
 #   role_arn        = aws_iam_role.eks_pod.arn
 # }
+
+module "istio" {
+  source  = "truemark/istio/kubernetes"
+  version = "0.0.5"
+  # insert the 1 required variable here
+  vpc_id = "Module.Network.vpc_id"
+  # depends_on = [ "aws eks update-kubeconfig --region us-east-1 --name demo_eks_cluster" ]
+}
+
+resource "null_resource" "update_kubeconfig" {
+    depends_on = [ aws_eks_cluster.eks_main_cluster ]
+  # This null resource will run the aws eks update-kubeconfig command
+  # before applying the Istio module.
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region us-east-1 --name demo_eks_cluster"
+  }
+}
