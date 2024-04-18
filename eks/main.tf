a390ad6681270dc3688c73dfd3c8791ea115f732
HarishDemoGit <harishdurai2@gmail.com>
2024-04-18 11:32:21 +0530

    demo21

--- eks/main.tf
+++ eks/main.tf
@@ -38,7 +38,8 @@ resource "aws_eks_node_group" "eks_nodeGroup" {
   cluster_name = var.eks_cluster_name
   node_group_name = var.eks_nodeGroup_name
   node_role_arn = aws_iam_role.eks_nodeGroup.arn
-  subnet_ids = toset(var.subnet_ids)
+  # subnet_ids = toset(var.subnet_ids)
+  subnet_ids = split(",", var.subnet_ids)
 
   scaling_config {
     desired_size = 1
