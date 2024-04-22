e5f180128ab9bf4dc90c2a082e2fb13e6c7b3ba2
HarishDemoGit <harishdurai2@gmail.com>
2024-04-22 13:07:40 +0530

    testing 4

--- Variables.tf
+++ Variables.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {
-   description = "aws account access key"  
-}
+# variable "aws_access_key" {
+#    description = "aws account access key"  
+# }
 
-variable "aws_secret_key" {
-    description = "aws account secret key" 
-}
+# variable "aws_secret_key" {
+#     description = "aws account secret key" 
+# }
 
